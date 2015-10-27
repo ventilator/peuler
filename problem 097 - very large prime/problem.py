8349b07a156483e7628608b7c7120724504218bb
ventilator <der_ventilator@web.de>
2015-10-27 17:36:41 +0100

    p97 created but not solved

---
+++ problem 097 - very large prime/problem.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 27 17:33:13 2015
+
+@author: venti
+
+
+
+The first known prime found to exceed one million digits was discovered in 1999, and is a Mersenne prime of the form 26972593−1;
+ it contains exactly 2,098,960 digits. Subsequently other Mersenne primes, of the form 2p−1, have been found which contain more digits.
+
+However, in 2004 there was found a massive non-Mersenne prime which contains 2,357,207 digits: 28433×27830457+1.
+
+Find the last ten digits of this prime number.
+
+"""
+
+prime = 28433 * 2**7830457 + 1
+str_prime = str(prime)
+print(str_prime[-10:-1])
