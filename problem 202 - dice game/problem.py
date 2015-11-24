b97c307bbee415ecf7b9c6957e2e1aa8ce4bf50a
ventilator <der_ventilator@web.de>
2015-11-24 17:56:39 +0100

    dices and stuff

--- problem 202 - dice game/problem.py
+++ problem 202 - dice game/problem.py
@@ -23,6 +23,7 @@ import time
 start_time = time.time()        
     
 from collections import namedtuple
+import matplotlib.pyplot as plt
 
 dice = namedtuple("dice", "count sides outcomes")    
 pyramidal = dice(9, [1,2,3,4], [])
@@ -33,11 +34,94 @@ cubical = dice(2, [1,2,3,4,5,6], [])
 
 import itertools
 
+def plotit(data, data2):
+    with plt.xkcd():
+    # Based on "Stove Ownership" from XKCD by Randall Monroe
+    # http://xkcd.com/418/
+
+        fig = plt.figure()
+#        ax = fig.add_axes((0.1, 0.2, 0.8, 0.7))
+#        ax.spines['right'].set_color('none')
+#        ax.spines['top'].set_color('none')
+#        plt.xticks([])
+#        plt.yticks([])
+#        ax.set_ylim([-30, 10])
+#    
+#        data = np.ones(100)
+#        data[70:] -= np.arange(30)
+    
+#        plt.annotate(
+#            'THE DAY I REALIZED\nI COULD COOK BACON\nWHENEVER I WANTED',
+#            xy=(70, 1), arrowprops=dict(arrowstyle='->'), xytext=(15, -10))
+    
+        x,y = map(list, zip(*data))    
+        x2,y2 = map(list, zip(*data2))    
+        
+        plt.scatter(x, y, marker = "s", c="b", label = "cubical dices")
+        plt.scatter(x2, y2, marker = "v", c="r", label = "pyramidal dices")    
+        
+        plt.legend(loc="upper left")
+        plt.xlabel('Augensumme')
+        plt.ylabel('probability')
+#        fig.text(
+#            0.5, 0.05,
+#            '"Stove Ownership" from xkcd by Randall Monroe',
+#            ha='center')
+    
+        # Based on "The Data So Far" from XKCD by Randall Monroe
+        # http://xkcd.com/373/
+    
+#        fig = plt.figure()
+#        ax = fig.add_axes((0.1, 0.2, 0.8, 0.7))
+#        ax.bar([-0.125, 1.0-0.125], [0, 100], 0.25)
+#        ax.spines['right'].set_color('none')
+#        ax.spines['top'].set_color('none')
+#        ax.xaxis.set_ticks_position('bottom')
+#        ax.set_xticks([0, 1])
+#        ax.set_xlim([-0.5, 1.5])
+#        ax.set_ylim([0, 110])
+#        ax.set_xticklabels(['CONFIRMED BY\nEXPERIMENT', 'REFUTED BY\nEXPERIMENT'])
+#        plt.yticks([])
+#    
+#        plt.title("CLAIMS OF SUPERNATURAL POWERS")
+#    
+#        fig.text(
+#            0.5, 0.05,
+#            '"The Data So Far" from xkcd by Randall Monroe',
+#            ha='center')
+    
+    plt.show()
+
+
+def probability(eyesum, count, sides):
+    if count == 1:
+        if ((eyesum > 0) and (eyesum <= sides)):
+            return 1/sides
+        else:
+            return 0
+    else:        
+        summe = 0
+        for i in range(1, eyesum - count + 1 + 1):
+            summe += probability(i, 1, sides) * probability(eyesum-i, count-1, sides) 
+        return summe    
+        
+
+def calc_probability(dice):
+    for i in range(dice.count, max(dice.sides)*dice.count + 1):
+        dice.outcomes.append([i, probability(i, dice.count, max(dice.sides))])
+        
+
 def solve_problem():
-    print(pyramidal.count)
-    pyramidal.outcomes.append(list(itertools.product(pyramidal.sides, repeat=pyramidal.count)))
-    pyramidal.outcomes.append(list(map(sum, pyramidal.outcomes)))
-    print(pyramidal.outcomes)
+    calc_probability(cubical)
+    calc_probability(pyramidal)    
+    
+    
+     
+    # plotit(pyramidal.outcomes)
+    # plotit(cubical.outcomes)
+    plotit(cubical.outcomes, pyramidal.outcomes)    
+    # pyramidal.outcomes.append(list(map(sum, pyramidal.outcomes)))
+    # print(pyramidal.outcomes)
         
     return 0
     
